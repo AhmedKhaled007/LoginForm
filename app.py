8ac5f79bb78d44b680ae2b2a2f6e06e903afa62f
Ahmed Khaled Habiba <ahmed.habiba4720@gmail.com>
2021-05-19 10:45:48 +0200

    add registration form

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
-
+from flask import Flask, request
+from flask.templating import render_template
+from wtforms import Form, StringField, PasswordField, validators
 
 app = Flask(__name__)
 
@@ -7,3 +8,26 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello"
+
+
+class RegistrationForm(Form):
+    name = StringField('Name', validators=[
+                       validators.InputRequired(),
+                       validators.Length(min=3, max=64)
+                       ])
+    email = StringField('Email', validators=[
+                        validators.InputRequired(), validators.Email()])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', 'Password do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        pass
+    else:
+        return render_template('register.html', form=form)
