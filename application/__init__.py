1ef452260ea36e3abf48341bb9fbb3989f7e41dd
Ahmed Khaled Habiba <ahmed.habiba4720@gmail.com>
2021-05-20 13:40:35 +0200

    add procfile and wsgi

--- application/__init__.py
+++ application/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import app_config
-
+import os
 db = SQLAlchemy()
 
 
@@ -9,11 +9,11 @@ def create_app():
     """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object(app_config['development'])
-
     db.init_app(app)
 
     with app.app_context():
         from . import views  # Import routes
+        print(app.config['SQLALCHEMY_DATABASE_URI'])
         db.create_all()  # Create sql tables for our data models
 
     return app
---
+++ wsgi.py
@@ -0,0 +1,8 @@
+from application import create_app
+from dotenv import load_dotenv
+
+app = create_app()
+load_dotenv('.env')
+
+if __name__ == '__main__':
+    app.run()
